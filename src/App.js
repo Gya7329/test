f086372ffc3e78267b584a312c8fc95fb3ecfe35
gya7329 <gya7329@gmail.com>
2022-09-12 19:25:47 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -4,30 +4,33 @@ import TodoList from "./components/TodoList";
 
 function App() {
   useEffect(() => {
-    fetch("https://apiv2.shiprocket.in/v1/external/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods":
-          "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Expose-Headers": "*",
-        "Access-Control-Max-Age": "3600",
-        "Access-Control-Request-Headers": "*",
-        "Access-Control-Request-Method": "*",
-        "Access-Control-Request-Credentials": "true",
-        "Access-Control-Request-Expose-Headers": "*",
-        "Access-Control-Request-Method": "*",
-      },
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://apiv2.shiprocket.in/v1/external/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+            "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+          "Access-Control-Allow-Credentials": "true",
+          "Access-Control-Expose-Headers": "*",
+          "Access-Control-Max-Age": "3600",
+          "Access-Control-Request-Headers": "*",
+          "Access-Control-Request-Method": "*",
+          "Access-Control-Request-Credentials": "true",
+          "Access-Control-Request-Expose-Headers": "*",
+          "Access-Control-Request-Method": "*",
+        },
 
-      body: JSON.stringify({
-        email: "amitsharma199938@gmail.com",
-        password: "Qwerty@199938",
-      }),
-    }).then((res) => {
+        body: JSON.stringify({
+          email: "amitsharma199938@gmail.com",
+          password: "Qwerty@199938",
+        }),
+      }
+    ).then((res) => {
       const result = res.json();
       console.log(result);
     });
